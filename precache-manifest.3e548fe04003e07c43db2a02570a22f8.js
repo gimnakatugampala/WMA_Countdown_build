d3e2cad81083abd3a4b946d90aba62a3fac641a6
gimnakatugampala <gimnakatugampala1@gmail.com>
2022-11-02 22:51:12 +0530

    first commit

---
+++ precache-manifest.3e548fe04003e07c43db2a02570a22f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd104af810b74cc566715f2e8a1b6fcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36f0ebefd134755f43af",
+    "url": "/static/css/main.9516270a.chunk.css"
+  },
+  {
+    "revision": "ddc6dc8fd2a814009e63",
+    "url": "/static/js/2.fffe78a6.chunk.js"
+  },
+  {
+    "revision": "36f0ebefd134755f43af",
+    "url": "/static/js/main.b760ece4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
